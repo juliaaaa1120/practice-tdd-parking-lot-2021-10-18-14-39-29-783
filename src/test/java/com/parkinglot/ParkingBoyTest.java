f2f658191b5476aa202b577afca448993285a62d
Julia Ng <ngju@oocl.com>
2021-12-03 16:38:51 +0800

    [ParkingLot][JuliaNg] - feat: Create Test Case - return ticket given parking boy manage 1 parking lot

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -1,2 +1,20 @@
-package com.parkinglot;public class ParkingBoyTest {
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingBoyTest {
+    @Test
+    void should_return_ticket_when_park_car_given_standard_parking_boy_manage_one_parking_lot_and_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        //when
+        Ticket ticket = parkingLot.park(new Car());
+
+        //then
+        assertNotNull(ticket);
+    }
 }
