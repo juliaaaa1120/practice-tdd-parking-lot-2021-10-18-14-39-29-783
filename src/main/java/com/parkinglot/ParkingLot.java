96f5a3f3af00518bfec51bccaf6ba3db491f48ca
Julia Ng <ngju@oocl.com>
2021-12-03 14:00:57 +0800

    [ParkingLot][JuliaNg] - feat: Completed Test Case - return ticket when park

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,7 +1,12 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+
 public class ParkingLot {
     public Ticket park(Car car) {
-        return null;
+        HashMap<Ticket, Car> ticketCarMap = new HashMap<>();
+        Ticket ticket = new Ticket();
+        ticketCarMap.put(ticket, car);
+        return ticket;
     }
 }
